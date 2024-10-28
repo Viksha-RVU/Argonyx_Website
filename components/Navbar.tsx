9f473b84831d2eda1ddb015e4048d6031d8a2804
Kautilya DK <kautilyadk@gmail.com>
2024-10-28 22:22:51 +0530

    final changes 2.0

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
     <div className="container flex h-14 max-w-screen-2xl items-center pt-2">
       <div className="flex justify-evenly md:justify-start items-center space-x-2 w-full">
         <a href="https://rvu.edu.in/" target="_blank" className="px-3">
-          <Image src={RvuLogo} alt="rvu_icon" height={100} width={140} />
+          <Image src={RvuLogo} alt="rvu_icon" height={90} width={130} />
         </a>
         <div className="w-full flex md:justify-end items-center">
           <a
@@ -29,11 +29,11 @@ function Navbar() {
               width={130}
             />
           </a>
-          <Image src={XLogo} alt="x" height={10} width={30} />
+          <Image src={XLogo} alt="x" height={15} width={15} />
           <a href="https://www.instagram.com/ieee.rvu" target="_blank" className="md:px-3">
-            <Image src={IEEELogo} alt="iee_icon" height={80} width={80} />
+            <Image src={IEEELogo} alt="iee_icon" height={130} width={190} />
           </a>
-          <Image src={XLogo} alt="x" height={10} width={30} />
+          <Image src={XLogo} alt="x" height={15} width={15} />
           <a
             href="https://www.instagram.com/neuralnexus_rvu"
             target="_blank"
