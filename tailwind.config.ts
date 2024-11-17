bc6b1efeb3a761723adb2b884d82bac2ca76ed06
Kautilya DK <kautilyadk@gmail.com>
2024-11-17 23:24:59 +0530

    fixed some issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,23 +74,12 @@ const config: Config = {
   					height: '0'
   				}
   			},
-  			'shimmer-slide': {
-  				to: {
-  					transform: 'translate(calc(100cqw - 100%), 0)'
-  				}
-  			},
-  			'spin-around': {
-  				'0%': {
-  					transform: 'translateZ(0) rotate(0)'
+  			'shiny-text': {
+  				'0%, 90%, 100%': {
+  					'background-position': 'calc(-100% - var(--shiny-width)) 0'
   				},
-  				'15%, 35%': {
-  					transform: 'translateZ(0) rotate(90deg)'
-  				},
-  				'65%, 85%': {
-  					transform: 'translateZ(0) rotate(270deg)'
-  				},
-  				'100%': {
-  					transform: 'translateZ(0) rotate(360deg)'
+  				'30%, 60%': {
+  					'background-position': 'calc(100% + var(--shiny-width)) 0'
   				}
   			},
   			pulse: {
@@ -113,8 +102,7 @@ const config: Config = {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'shimmer-slide': 'shimmer-slide var(--speed) ease-in-out infinite alternate',
-  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
+  			'shiny-text': 'shiny-text 8s infinite',
   			pulse: 'pulse var(--duration) ease-out infinite',
   			'background-position-spin': 'background-position-spin 3000ms infinite alternate'
   		}
